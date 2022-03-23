e70fa206e93fa632c13162aabb71acb6652d45fe
Liron Shriki <lironshriki757@gmail.com>
2022-03-23 23:26:35 +0200

    finished FAQ section

--- mint-client/src/themes/FAQ/FAQ.jsx
+++ mint-client/src/themes/FAQ/FAQ.jsx
@@ -1,45 +1,86 @@
-import React from "react";
-import { styled } from "@mui/material";
+import React, { useState } from "react";
+import { Grid, styled, Typography } from "@mui/material";
 import ToggleInfo from "./ToggleInfo";
+import data from "./data.json";
 
-const Screen = styled('div')({
-  width: '100vw',
-  minHeight: '100vh',
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center'
+const Screen = styled("div")({
+  width: "100vw",
+  minHeight: "100vh",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  backgroundImage: "url(assets/DesktopSkyBackground.png)",
 });
 
 const Title = styled("div")({
-  fontSize: '3rem',
-  fontWeight: 'bold',
-  display: 'flex',
-  flexDirection: 'column',
-  textAlign: 'center',
-  marginTop: '20vh',
-  marginBottom: '5vh',
-  color: '#F1F1F1'
+  fontSize: "3rem",
+  fontWeight: "bold",
+  display: "flex",
+  flexDirection: "column",
+  textAlign: "center",
+  marginTop: "17vh",
+  marginBottom: "5vh",
+  color: "#F1F1F1",
 });
 
-const TitleUnderLine = styled('div')({
-  border: '4px solid',
-  color: '#FFCC00',
-  width: '50px',
+const TitleUnderLine = styled("div")({
+  border: "4px solid",
+  color: "#FFCC00",
+  width: "50px",
+  alignSelf: "center",
+  marginTop: "15px",
+});
+
+const RightsResevedSection = styled(Grid)({
+  display: 'flex',
+  flexDirection: 'column',
+});
+
+const RightsReservedText = styled(Typography)({
+  color: "#F1F1F1",
   alignSelf: 'center',
-  marginTop: '15px'
-})
+  justifySelf: 'center',
+  height: '--webkit-fill-available'
+});
+
+const SeperationLine = styled("div")({
+  width: "80vw",
+  marginBottom: '30px',
+  borderBottom: "1px solid #707070",
+});
+
+const ContentContainer = styled(Grid)({
+  height: "58vh",
+});
 
 const FAQ = () => {
+  const [openKey, setOpenKey] = useState(-1)
+
   return (
-    <Screen style={{ backgroundImage: "assets/DesktopSkyBackground.png" }}>
+    <Screen>
       <Title>
         FAQ
-        <TitleUnderLine/>
+        <TitleUnderLine />
       </Title>
+      <ContentContainer>
+        {data.data.map((info, index) => {          
+          return(
+          <ToggleInfo
+            id={index}
+            openKey={openKey}
+            title={info.title}
+            subTitle={info.subTitle}
+            content={info.content}
+            width={"20vw"}
+            setOpenKey={setOpenKey}
+          />
+        )})}
+      </ContentContainer>
 
-      <ToggleInfo width={'20vw'}/>
-      <ToggleInfo width={'20vw'}/>
-      <ToggleInfo width={'20vw'}/>
+      <RightsResevedSection>
+        <SeperationLine />
+        <RightsReservedText>{"© All Rights Reserved."}</RightsReservedText>
+      </RightsResevedSection>
     </Screen>
   );
 };
